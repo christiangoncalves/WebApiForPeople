c12780fc8993d07aff941fc29654f888bae8cc2b
christiangoncalves <christiangoncalves19@gmail.com>
2020-02-10 12:38:18 -0300

    Tratamento de Erros

--- WebApiForPeople/Controllers/FuncionariosController.cs
+++ WebApiForPeople/Controllers/FuncionariosController.cs
@@ -48,38 +48,104 @@ namespace WebApiForPeople.Controllers
         }
 
         [HttpPost]
-        public void Create([FromBody] JObject a)
+        public JObject Create([FromBody] JObject a)
         {
-            Endereco endereco = new Endereco(a);
-            FuncionarioModels func = new FuncionarioModels((int)a["id"], (string)a["nome"], (string)a["cpf"], (int)a["idade"], endereco);
-            func.dataCriacao = DateTime.Now;
-            funcionarios.Add(func);
+            try
+            {
+                Endereco endereco = new Endereco(a);
+
+                FuncionarioModels func = new FuncionarioModels((int)a["id"], (string)a["nome"], (string)a["cpf"], (int)a["idade"], endereco);
+                func.dataCriacao = DateTime.Now;
+                funcionarios.Add(func);
+
+                String success = "Funcionario criado com sucesso";
+                JObject successJson = new JObject();
+                successJson["Success"] = success;
+                return successJson;
+            }
+            catch (Exception e)
+            {
+                String error = e.Message;
+                JObject errorJson = new JObject();
+                errorJson["Error"] = error;
+                errorJson["Ação"] = "Verifique se os campos do Json são iguais aos campos permitidos pela requisição!";
+                return errorJson;
+            }
+            
         }
 
         [HttpPost]
-        public void Destroy([FromBody] int id)
+        public JObject Destroy([FromBody] int id)
         {
+            bool aux = false;
+
             for (int i = 0; i < funcionarios.Count; i++)
             {
                 if (funcionarios[i].id == id)
                 {
                     funcionarios.RemoveAt(i);
+                    aux = true;
+
+                    String success = "Funcionario deletado com sucesso";
+                    JObject successJson = new JObject();
+                    successJson["Success"] = success;
+                    return successJson;
+
                 }
             }
+
+            if (!aux)
+            {
+                String e = "Funcionário nao encontrado!";
+                JObject error = new JObject();
+                error["Error"] = e;
+
+                return error;
+            }
+
+            return null;
         }
 
         [HttpPost]
-        public void Update([FromBody] JObject a)
+        public JObject Update([FromBody] JObject a)
         {
-            Endereco endereco = new Endereco(a);
-
-            for (int i = 0; i < funcionarios.Count; i++)
+            try
             {
-                if (funcionarios[i].id == (int)a["id"])
+                Endereco endereco = new Endereco(a);
+                bool aux = false;
+
+                for (int i = 0; i < funcionarios.Count; i++)
                 {
-                    funcionarios[i].update((int)a["id"], (string)a["nome"], (string)a["cpf"], (int)a["idade"], endereco);
+                    if (funcionarios[i].id == (int)a["id"])
+                    {
+                        funcionarios[i].update((int)a["id"], (string)a["nome"], (string)a["cpf"], (int)a["idade"], endereco);
+                        aux = true;
+
+                        String success = "Funcionario Atualizado com sucesso";
+                        JObject successJson = new JObject();
+                        successJson["Success"] = success;
+                        return successJson;
+                    }
                 }
+                if (!aux)
+                {
+                    String e = "Funcionário nao encontrado!";
+                    JObject error = new JObject();
+                    error["Error"] = e;
+
+                    return error;
+                }
+                return null;
             }
+            catch (Exception e)
+            {
+                String error = e.Message;
+                JObject errorJson = new JObject();
+                errorJson["Error"] = error;
+                errorJson["Ação"] = "Verifique se os campos do Json são iguais aos campos permitidos pela requisição!";
+                return errorJson;
+            }
+            
         }
 
         [NonAction]
